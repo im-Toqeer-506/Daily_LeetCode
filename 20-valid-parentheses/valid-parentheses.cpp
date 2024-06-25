a0f76dae5bcefac3de05070716d420bb0732d969
Muhammad Toqeer <162108644+muhammadtoqeer32@users.noreply.github.com>
2024-06-25 22:45:25 +0500

    Time: 0 ms (100.00%) | Memory: 7.4 MB (86.09%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> Stack;  // Use char instead of int
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '[' || s[i] == '{' || s[i] == '(') {
+                Stack.push(s[i]);
+            } else {
+                if (Stack.empty())
+                    return false;
+                c = Stack.top(); 
+                Stack.pop();
+                if ((s[i] == '}' && c != '{') || 
+                    (s[i] == ')' && c != '(') || 
+                    (s[i] == ']' && c != '[')) {
+                    return false;
+                }
+            }
+        }
+        return Stack.empty();
+    }
+};
