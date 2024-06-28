9b2fa40460a8670d4a53925be90472ed7cbc467d
Muhammad Toqeer <162108644+muhammadtoqeer32@users.noreply.github.com>
2024-06-28 16:37:44 +0500

    Time: 90 ms (37.04%) | Memory: 27 MB (65.80%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string result="";
+        for(auto c: s){
+            if(c=='*')
+            {
+              if(!result.empty())
+              {
+                result.pop_back();
+              }
+            }
+            else
+            {
+              result.push_back(c);
+            }
+        }
+
+        return result;
+
+    }
+};
