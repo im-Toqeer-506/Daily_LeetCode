23afe4c7bdeaef4988f2aae177da1476b9bfd5f0
Muhammad Toqeer <162108644+muhammadtoqeer32@users.noreply.github.com>
2024-07-02 16:38:21 +0500

    Time: 22 ms (30.94%) | Memory: 14.2 MB (16.17%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+     int lengthOfLongestSubstring(string s) {
+        int maxWindow = 0;
+        unordered_set<int>charSet;
+        int prev= 0;
+
+
+        for(int i =0;i<s.length();i++){
+            if(charSet.count(s[i]) ==0){
+            charSet.insert(s[i]);
+            maxWindow = max(maxWindow, i-prev+1);
+            }else{
+                while(charSet.count(s[i]) != 0){
+
+                
+                charSet.erase(s[prev]);
+                prev += 1;
+                }
+                charSet.insert(s[i]);
+
+               }
+        }
+            return maxWindow;
+        } 
+};
