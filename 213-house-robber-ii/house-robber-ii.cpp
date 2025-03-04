157cb52c5cc424733c464a59a666c546490fe66a
Muhammad Toqeer <162108644+imToqeer@users.noreply.github.com>
2025-03-04 23:29:06 +0500

    Time: 0 ms (100.00%) | Memory: 10.2 MB (77.00%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -13,6 +13,7 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0]; 
+        // (phely first ko chorna hai ,then dosery ko chorna hai)
         return max(robLinear(nums, 0, n-2), robLinear(nums, 1, n-1));
     }
 };
