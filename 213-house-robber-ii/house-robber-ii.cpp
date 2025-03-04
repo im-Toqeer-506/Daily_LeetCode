7b44b6ae5b01eac74a23d5c45743fa4df4ddb1e7
Muhammad Toqeer <162108644+imToqeer@users.noreply.github.com>
2025-03-04 23:28:12 +0500

    Time: 0 ms (100.00%) | Memory: 10.3 MB (77.00%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int robLinear(vector<int>& nums, int s, int e) {
+        int p2 = 0; 
+        int p1 = 0; 
+        for (int i = s; i <= e; i++) {
+            int curr = max(p1, p2 + nums[i]); 
+            p2 = p1; 
+            p1 = curr;  
+        }
+        return p1; 
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0]; 
+        return max(robLinear(nums, 0, n-2), robLinear(nums, 1, n-1));
+    }
+};
