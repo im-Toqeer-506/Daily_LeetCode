4f2d97196dd89bfb6da2206aa4a18c7b4901859a
Muhammad Toqeer <162108644+im-Toqeer-506@users.noreply.github.com>
2025-08-01 21:19:05 +0500

    Time: 71 ms (76.75%) | Memory: 98.6 MB (43.36%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        // we are using this as a sliding window!
-        unordered_set<int> st;
-        int i = 0, j = 0;
-        while (j < nums.size()) {
-            //if window side exceed k then shrink it 
-            if (abs(i - j) > k) 
-            {
-                st.erase(nums[i]);
-                i++;//move the window one step farwed to shrink from left
-            }
-            //if number already in window 
-            //then we font number inn the same range of k
-            if (st.find(nums[j]) != st.end()) {
-                return true;
-            }
-            st.insert(nums[j]);
-            j++;//move ethe right farwed to increrse win
-        }
-        return false;
+       unordered_map<int, int> map;
+       for(int i = 0; i < nums.size(); i++) {
+
+           if(map.count(nums[i]) > 0 && abs(map[nums[i]] - i) <= k) {
+               return true;
+           }
+           map[nums[i]] = i;
+       }
+       return false;
     }
 };
+
+
+// map.count(nums[i])
