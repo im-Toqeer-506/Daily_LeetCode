c0c0e48361f21660246b89935e4ff5de234b4471
Muhammad Toqeer <162108644+im-Toqeer-506@users.noreply.github.com>
2025-08-15 00:26:02 +0500

    Time: 2 ms (4.21%) | Memory: 8.3 MB (46.57%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int memorization(int n, vector<int>& memo) {
-        if (n < 0)
-            return 0;
-        if (n == 1 || n == 2)
-            return 1;
-        if (memo[n] != -1)
-            return memo[n];
-        memo[n] = memorization(n - 1, memo) + memorization(n - 2, memo) +
-                  memorization(n - 3, memo);
-        return memo[n];
+    int fibo(int n, unordered_map<int, int>& map) {
+        map[0] = 0;
+        map[1] = 1;
+        map[2] = 1;
+        //base case 
+        if (map.find(n) != map.end())
+            return map[n];
+
+        for (int i = 3; i <= n; i++) {
+            map[n] = fibo(i - 1, map) + fibo(i - 2, map) + fibo(i - 3, map);
+        }
+
+        return map[n];
     }
     int tribonacci(int n) {
-        vector<int> memo(n + 1, -1);
-        return memorization(n, memo);
+        unordered_map<int, int> map;
+        return fibo(n, map);
     }
 };
+// 0,1,1,2,4,7,13....
